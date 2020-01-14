dfa44a1168f6a7c2b9dee98d63d03af6a31978ca
Subhiksha Lakshmi G <subhikshalakshmi.g@cognizant.com>
2020-01-14 13:33:06 +0530

    ver1.0

---
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.User;
+
+@Controller
+public class SimpleController {
+	/*
+	 * @Value("${spring.application.name}") String appName;
+	 * 
+	 * @GetMapping("/") public String homePage(Model model) {
+	 * model.addAttribute("appName", appName); return "home"; }
+	 */
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user-data");
+		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+}
